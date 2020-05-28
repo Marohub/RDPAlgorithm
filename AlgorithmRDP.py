64cf0ed744d43191b0d40d09d3be4f017c9d9ac1
Kozlowski, Marek <mkozlowski124@gmail.com>
2020-05-28 23:52:56 +0200

    Correct order of methods; add points generator

--- AlgorithmRDP.py
+++ AlgorithmRDP.py
@@ -5,6 +5,7 @@ import sys
 
 import matplotlib.pyplot as plt
 
+
 def draw_plots(rdp_points, original_points):
     x_axis_RDP, y_axis_RDP = zip(*rdp_points)
     x_axis_original, y_axis_original = zip(*original_points)
@@ -12,8 +13,20 @@ def draw_plots(rdp_points, original_points):
     plt.plot(x_axis_original, y_axis_original)
     plt.show()
 
+
+def function(x):
+    return math.sin(3*math.pi*x)*x
+
+
+def get_points():
+    points = []
+    for x in np.arange(0.0, 5.0, 0.01):
+        points.append([x, function(x)])
+    return points
+
+
 def main():
-    points = np.array([[0.0, 0.0], [1.0, 0.1], [2.0, -0.1], [3.0, 5.0], [4.0, 6.0], [5.0, 7.1], [6.0, 8.1], [7.0, 9.0], [8.0, 9.0], [9.0, 9.0]])
+    points = np.array(get_points())
     epsilon = 1.0
     original_rdp = rdp(points, epsilon)
     custom_rdp = RDP(points, epsilon).run()
@@ -23,10 +36,33 @@ def main():
 
 
 class RDP:
+
     def __init__(self, points, epsilon):
         self.rdp_array = []
         self.points = points
         self.epsilon = epsilon
+
+    def get_point(self, index):
+        return self.points[index][0], self.points[index][1]
+
+    def perpendicular_distance(self, A, B, C, index):
+        x, y = self.get_point(index)
+        numerator = abs(A*x+B*y+C)
+        denominator = math.sqrt(A**2 + B**2)
+        return numerator/denominator
+
+    def get_line_coefficients(self, start_index, end_index):
+        # Ax + By + C = 0
+        A = C = 0
+        B = -1
+        x1, y1 = self.get_point(start_index)
+        x2, y2 = self.get_point(end_index)
+        if x1 == x2:
+            A = x2
+        else:
+            A = (y2 - y1)/(x2 - x1)
+            C = y1 - A*x1
+        return A, B, C
 
     def find_furthest(self, start_index, end_index):
         A, B, C = self.get_line_coefficients(start_index, end_index)
@@ -41,14 +77,11 @@ class RDP:
         else:
             return -1
 
-    def get_point(self, index):
-        return self.points[index][0], self.points[index][1]
-
-    def add_to_rdp_array(self,index):
+    def add_to_rdp_array(self, index):
         x, y = self.get_point(index)
-        self.rdp_array.append([x,y])
+        self.rdp_array.append([x, y])
 
-    def algorithm(self, start_index, end_index) :
+    def algorithm(self, start_index, end_index):
         next_index = self.find_furthest(start_index, end_index)
         if next_index > 0:
             if start_index != next_index:
@@ -57,31 +90,12 @@ class RDP:
             if end_index != next_index:
                 self.algorithm(next_index, end_index)
 
-    def perpendicular_distance(self, A, B, C, index):
-        x, y = self.get_point(index)
-        numerator = abs(A*x+B*y+C)
-        denominator = math.sqrt(A**2 + B**2)
-        return numerator/denominator
-    
-    def get_line_coefficients(self, start_index, end_index):
-        # format y = ax + b
-        # Ax + By + C = 0
-        A = C = 0
-        B = -1
-        x1, y1 = self.get_point(start_index)
-        x2, y2 = self.get_point(end_index)
-        if x1 == x2:
-            A = x2
-        else :
-            A = (y2 - y1)/(x2 - x1)
-            C = y1 - A*x1
-        return A, B, C
-
     def run(self):
         self.add_to_rdp_array(0)
         self.algorithm(0, len(self.points)-1)
         self.add_to_rdp_array(len(self.points)-1)
         return np.array(self.rdp_array)
+
 
 if __name__ == "__main__":
     sys.exit(main())
